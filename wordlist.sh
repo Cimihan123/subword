7fe84f017ef29dc9ade6da87629814098375a0af
Kiran Ghimire <47425314+Cimihan123@users.noreply.github.com>
2020-08-18 13:29:45 +0545

    Update wordlist.sh

--- wordlist.sh
+++ wordlist.sh
@@ -19,3 +19,4 @@ cat curled.txt | tok | tr '[:upper:]' '[:lower:]' | sort -u | tee -a wordlist.tx
 gau 200_domains_frame.io.txt | head -n 1000 | fff -s 200 -s 404
 
 
+#gau $1| unfurl -u keys | tee -a wordlist.txt ; gau $1 | unfurl -u paths|tee -a ends.txt; sed 's#/#\n#g' ends.txt  | sort -u | tee -a wordlist.txt | sort -u ;rm ends.txt  | sed -i -e 's/\.css\|\.png\|\.jpeg\|\.jpg\|\.svg\|\.gif\|\.wolf\|\.bmp//g' wordlist.txt
