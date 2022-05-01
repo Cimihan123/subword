4379ee66ee7a3bc24e3fba1a7c0808b1f62b4c60
Kiran Ghimire <47425314+Cimihan123@users.noreply.github.com>
2022-05-01 17:26:41 +0545

    updated

--- download.sh
+++ download.sh
@@ -38,4 +38,5 @@ go install -v github.com/projectdiscovery/httpx/cmd/httpx@latest
 #nuclei
 go install -v github.com/projectdiscovery/nuclei/v2/cmd/nuclei@latest
 
-
+#subfinder
+go install -v github.com/projectdiscovery/subfinder/v2/cmd/subfinder@latest
